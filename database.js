142b31d3a33326669c204309ea4c99137ac796c6
Tan Jian Sin <e0003810@u.nus.edu>
2019-04-10 11:17:10 -0400

    bug fix

--- database.js
+++ database.js
@@ -20,7 +20,7 @@ var pool = adb.createPool(process.env.DB_CONNECT,{ min : 1, max : 4 });
 
 
 function sanitize(unclean){
-   return adb.escape(unclean)
+   return mysql_real_escape_string(unclean)
 }
 
 /********************************************************************************/
@@ -72,6 +72,31 @@ function fixQuery(q)
    return q;
 }
 
+
+function mysql_real_escape_string (str) {
+   return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+       switch (char) {
+           case "\0":
+               return "\\0";
+           case "\x08":
+               return "\\b";
+           case "\x09":
+               return "\\t";
+           case "\x1a":
+               return "\\z";
+           case "\n":
+               return "\\n";
+           case "\r":
+               return "\\r";
+           case "\"":
+           case "'":
+           case "\\":
+           case "%":
+               return "\\"+char; // prepends a backslash to backslash, percent,
+                                 // and double/single quotes
+       }
+   });
+}
 
 
 /********************************************************************************/
