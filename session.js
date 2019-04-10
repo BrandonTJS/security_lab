def7ce974bbb9e36ff53066d7bdbe78a421161a4
Tan Jian Sin <e0003810@u.nus.edu>
2019-04-10 12:33:09 -0400

    edit template

--- session.js
+++ session.js
@@ -78,7 +78,7 @@ function displayLoginPage(req,res)
    return res.render("login", { userName: "",
 				password: "",
             loginError: "",
-            uptime: process.uptime()});
+            uptime: getUptime()});
 }
 
 
@@ -113,7 +113,7 @@ function handleLoginRequest1(req,res,next,err,data)
       return res.render("login", { userName : username,
 				      password : "",
 				      loginError : invalidUserNameErrorMessage,
-                  uptime: process.uptime() } );
+                  uptime: getUptime() } );
     }
    else {
       var userdata = data.rows[0];
@@ -121,7 +121,7 @@ function handleLoginRequest1(req,res,next,err,data)
 	 return res.render("login", { userName : username,
 				      password : "",
 				      loginError : invalidPasswordErrorMessage,
-                  uptime: process.uptime() } );
+                  uptime: getUptime() } );
        }
 
       req.session.userId = userdata.userId;
@@ -136,8 +136,24 @@ function comparePassword(fromdb,fromuser)
    return fromdb == fromuser;
 }
 
+String.prototype.toHHMMSS = function () {
+   var sec_num = parseInt(this, 10); // don't forget the second param
+   var hours   = Math.floor(sec_num / 3600);
+   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+   var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
+   if (hours   < 10) {hours   = "0"+hours;}
+   if (minutes < 10) {minutes = "0"+minutes;}
+   if (seconds < 10) {seconds = "0"+seconds;}
+   var time    = hours+':'+minutes+':'+seconds;
+   return time;
+}
 
+function getUptime(){
+   var time = process.uptime();
+    var uptime = (time + "").toHHMMSS();
+    return uptime;
+}
 
 /********************************************************************************/
 /*										*/
